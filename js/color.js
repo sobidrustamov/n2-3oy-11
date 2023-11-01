e8c68f8a7cdafc96f65822847921ba54186c513b
Sobid <rustamovsobid95@gmail.com>
2023-11-02 03:10:43 +0500

    ..

--- js/color.js
+++ js/color.js
@@ -1,7 +1,8 @@
-let color = "#" + Math.random().toString(16).slice(2, 8);
-console.log(color);
-let body = document.querySelector("body");
-body.innerHTML = color;
-body.style.background = color;
-
-
+function color() {
+  let color = "#" + Math.random().toString(16).slice(2, 8);
+  console.log(color);
+  let body = document.querySelector("body");
+  body.innerHTML = color;
+  body.style.background = color;
+}
+setInterval(color, 2000);
